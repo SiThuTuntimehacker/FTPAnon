fe2affebb5d48866a8786ffa5b2da1bca93e9dbc
SiThuTun <40698550+SiThuTuntimehacker@users.noreply.github.com>
2020-10-29 04:05:57 +0630

    Add files via upload

--- ftpanon.py
+++ ftpanon.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python
+#----colors----
+
+green='\033[1;92m'
 import ftplib
 import sys
+
+print '''\033[1;92m
+ ____  _ _____ _
+/ ___|(_)_   _| |__  _   _
+\___ \| | | | | '_ \| | | |
+ ___) | | | | | | | | |_| |
+|____/|_| |_| |_| |_|\__,_|
+\033[0;36m
+'''
 
 def anoncheck(ip):
 	try:
@@ -14,7 +26,8 @@ def anoncheck(ip):
 		return False
 
 if len(sys.argv) < 2:
-	print("Not enough arguments \n Usage: ftpcon.py hostname\n")
+	print("Not enough arguments \n Usage: ftpanon.py hostname or ip address\n")
+        print(" example :    ./ftpanon.py 209.122.238.110 ")
 	exit()
 else:
 	ip=sys.argv[1]
