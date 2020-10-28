74ff93b4993f14d1ca7bf39fcbd9e39d074c4ecb
SiThuTun <40698550+SiThuTuntimehacker@users.noreply.github.com>
2020-10-29 03:20:50 +0630

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!bin
+
+apt install vsftpd
+
+apt-get install vsftpd
+
+cd 
+cd /etc/
+rm -rf vsftpd.conf
+cd 
+cd FTPAnon
+cp -r vsftpd.conf /etc/
+cd
+
+service vsftpd start
+
+
+Thank For Using Tool
+Termux Pentest Page https://www.facebook.com/termux.myanmar.user/
